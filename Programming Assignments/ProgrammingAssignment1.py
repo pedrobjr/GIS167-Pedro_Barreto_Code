083f666010aec15fe7d9db8024bf61c778bc39f6
Pedro Barreto <pedro_barreto@redlands.edu>
2026-01-14 22:05:17 -0800

    k

---
+++ Programming Assignments/ProgrammingAssignment1.py
@@ -0,0 +1,10 @@
+first_name = 'Joe'
+last_name = 'Shmoe'
+age = 45
+miles = 3.5
+action = 'jumped'
+yelled = 'weeeee'
+print(f"Yesterday morning {first_name} {last_name} went on a run to celebrate his {age}th birthday. {first_name} ran {miles} miles"\
+f" and {action} over a dog and shouted {yelled}!")
+print(f"Yesterday morning {first_name.upper()} {last_name} went on a run to celebrate his {(age + 1)}th birthday. {first_name}"\
+f" ran {miles} miles and {action} over a dog and shouted {yelled}!")
