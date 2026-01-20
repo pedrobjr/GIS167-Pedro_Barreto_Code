3c66d6228658c1489d3b4bb34b8e51a14ca22726
Pedro Barreto <pedro_barreto@redlands.edu>
2026-01-20 12:22:37 -0800

    changes

--- Programming Assignments/ProgrammingAssignment2.py
+++ Programming Assignments/ProgrammingAssignment2.py
@@ -4,11 +4,9 @@ products = ['glasses','cruches','fan','cup','desk','ipod','phone','lamp','tv','d
 print(f'Length of first name list: {len(first)}\
 \nLength of last name list: {len(last)}\
 \nLength of products list: {len(products)}')
-
 for name in range(len(first)):
     print(f'{first[name]} {last[name]}: {products[name]}\n{first[name].lower()}.{last[name].lower()}@redlands.edu\
     \nDear {first[name]} {last[name]}, I have the following item available for sale: {products[name]}.\nIf you are interested please email me.\n')
-
 sorted_products = sorted(products)
 for name in sorted_products[:4]:
     print(name.upper())
